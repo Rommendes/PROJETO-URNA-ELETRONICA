9a0ac981e96a7bd72334cad780dfdc9805c368f2
Rosangela <96451367+Rommendes@users.noreply.github.com>
2023-02-11 18:34:18 -0300

    ..

--- images/script.js
+++ images/script.js
@@ -1,1 +1,7 @@
 //1º CRIAR UMA VARIÁVEL PARA CADA UMA DAS INFORMAÇÕES
+let seuVotoPara = document.querySelector('.divisao1-1 span');
+let cargo = document.querySelector('.divisao1-2 span');
+let descricao = document.querySelector('.divisao1-4');
+let aviso = document.querySelector('.divisao2');
+let lateralTela= document.querySelector('.divisao1-rightImg');
+let numeros = document.querySelector('.divisao1-3');
