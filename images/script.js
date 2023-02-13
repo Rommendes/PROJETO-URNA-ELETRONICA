32ae77864fc10f07a0c08c49d98f97e6f5a12001
Rosangela <96451367+Rommendes@users.noreply.github.com>
2023-02-13 16:27:21 -0300

    ...

--- images/script.js
+++ images/script.js
@@ -6,8 +6,47 @@ let aviso = document.querySelector('.divisao2');
 let lateralTela= document.querySelector('.divisao1-rightImg');
 let numeros = document.querySelector('.divisao1-3');
 
+let etapaAtual = 0;
+let numero = '';
+function comecarEtapa(){
+    let etapa = etapas[etapaAtual];
+
+    let numerosHtml = '';
+
+    for(let i = 0; i < etapa.numeros; i++){
+        if(i === 0){
+            numerosHtml += '<div class="numero pisca"></div>'
+        } else {
+            numerosHtml += '<div class="numero "></div>'
+        }
+       
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateralTela.innerHTML = '';
+    numeros.innerHTML = numerosHtml;
+}
+function atualizaInterface(){
+    alert('Finalizou de digitar o voto!')
+}
+
 function clicou (n){
-    alert('clicou em ' + n)
+    let numeroCandidato = document.querySelector('.numero.pisca');
+    if(numeroCandidato !== null){
+        numeroCandidato.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        numeroCandidato.classList.remove('pisca')
+        if(numeroCandidato.nextElementSibling !== null){
+            numeroCandidato.nextElementSibling.classList.add('pisca');
+        }else{
+            atualizaInterface();
+        }
+        
+    }
 }
 
 function branco(){
@@ -21,3 +60,4 @@ function corrige(){
 function confirma(){
     alert('Clicou em confirma')
 }
+comecarEtapa();
